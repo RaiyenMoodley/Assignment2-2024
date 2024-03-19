801e718386ec2b33daf94e5393b8daabe2b4c68b
Raiyen Moodley <raiyenm0@gmail.com>
2024-03-19 22:10:28 +0200

    Added relevant information

--- src/Entry.java
+++ src/Entry.java
@@ -1,3 +1,10 @@
+/**
+ * Entry class that stores each lines of Knowledge Base
+ * Raiyen Moodley
+ * MDLRAI001
+ * 19/03/2024
+ */
+
 public class Entry {
     public String term;
     public String statement;
@@ -9,6 +16,7 @@ public class Entry {
        this.confidenceScore = confidenceScore;
     }
 
+    //Get and Set Methods for instance variables.
     public String getTerm() {
         return term;
     }
